26fcd0d0f175b4c9195af553289bcf1c51af818a
KEN JONES <kjones67@gmail.com>
2020-01-25 00:11:36 -0600

    Created connection to Sql Server. Successfully checked for table existence and performed data deletion. Successfully read from sql server into a pandas dataframe. Successfully wrote from a pandas dataframe to sql server.

--- SqlConnection.py
+++ SqlConnection.py
@@ -4,6 +4,7 @@ import urllib
 import pandas as pd
 import pyodbc
 import sqlalchemy as sql
+from sqlalchemy.orm import sessionmaker
 
 # environment variables
 load_dotenv()
@@ -11,64 +12,58 @@ os.environ['MSSQL_DB'] = 'StockOddsDB'
 os.environ['MSSQL_TABLE'] = 'TestTable'
 
 # database connection parameters
-# db = os.environ.get('MSSQL_DB')
-# db_table = os.environ.get('MSSQL_TABLE')
-# server = os.getenv("MSSQL_SERVER")
-# username = os.getenv('MSSQL_USERNAME')
-# password = os.getenv('MSSQL_PASSWORD')
-
 database = 'StockOddsDB'
 db_table = 'TestTable'
 server = r'FRANKENSTEIN\SQLEXPRESS'
-# username = r'FRANKENSTEIN\Ken'
-# password = 'Skando!23Q'
 
+params = urllib.parse.quote_plus('DRIVER={ODBC Driver 13 for Sql Server};SERVER='+server+';DATABASE='
+                                    +database+";Trusted_Connection=yes;")
+
+# if using Sql Authentication (UID, PSWD)
 # params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER='+server+
 #                                  ';DATABASE='+db+';UID='+username+';PWD='+password)
 
-# params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER='+server+
-#                                  ';DATABASE='+db+';UID='+username+';PWD='+password)
+# create the sqlalchemy engine for efficient reading/writing to Sql Server
+engine = sql.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
-# engine = sql.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params, fast_executemany=True)
-# engine = sql.create_engine(f"mssql://{server}\\SQLEXPRESS/{db}?trusted_connection=yes")
-# engine = sql.create_engine(f'mssql://{server}/{db}?trusted_connection=yes')
-# engine = sql.create_engine(f'mssql+pyodbc://@{server}/{db}')
-# engine = sql.create_engine('mssql+pyodbc://@' + server + '/' + db + '?trusted_connection=yes&driver=ODBC+Driver+13+for+SQL+Server')
-# engine = sql.create_engine('mssql+pyodbc://@' + server + '/' + db + '?trusted_connection=yes&driver=ODBC+Driver+13+for+SQL+Server')
+# sqlalchemy uses 'sessions' to execute sql statements (as a "holding zone")
+# requires 'from sqlalchemy.orm import sessionmaker'
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
 
-# define the connection string
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; \
-                       SERVER=' + server + '; \
-                       DATABASE=' + database +'; \
-                       Trusted_Connection=yes;')
-stopper = True
+# create an instance of the Session class
+session = Session()
+
+# read data from the db
+def read_from_sql_server():
+    from_sql_df = pd.read_sql(f'SELECT * FROM {db_table}', engine)
+    return from_sql_df
+
 
 # write data to the db
-def write_to_db():
-    # clear existing data from the table
+def write_to_sql_server():
+    data = {'Symbol': ['AAPL', 'FB', 'NVDA'], 'ExDivDate': ['2020-01-01', '2020-02-01', '2020-03-01'],
+            'DivAmt': ['.1234', '.2345', '.3456']}
+
+    df = pd.DataFrame(data)
+    df.to_sql(db_table, con=engine, if_exists='append', index=False)
+
+
+def clear_table():
+    # clear existing data from the table if it exists
     delete_query = f'''IF Object_id('{db_table}') IS NOT NULL
                             DELETE FROM {db_table};'''
 
-    # create the connection cursor
-    cursor = cnxn.cursor()
-
-    cursor.execute(delete_query)
-    cnxn.commit()
-
-    data = {'Symbol': ['AAPL', 'FB', 'NVDA'], 'ExDivDate': ['2020-01-01', '2020-02-01', '2020-03-01'],
-          'DivAmt': ['.1234', '.2345', '.3456']}
-
-    df = pd.DataFrame(data)
-
-    # for index, row in df.iterrows():
-    #     cursor.execute(f'INSERT INTO {db_table}([Symbol], [ExDivDate], [DivAmt]')
-    #     cnxn.commit()
-
-    cursor.close()
-    cnxn.close()
+    session.execute(delete_query)
+    session.commit()
+    # not sure if i should be closing this here???
+    session.close()
 
 
 if __name__=='__main__':
     print("Program Entry")
-    write_to_db()
+    clear_table()
+    write_to_sql_server()
+    df_from_sql = read_from_sql_server()
+    print(df_from_sql)
     print('Program Exit')
